cd4c46ff45902c61ab625e37164f9e6d1bafd688
@jbrzozowski <jeremybrzozowski@gmail.com>
2020-10-20 15:59:52 -0700

    Update Data Analysis Queries

--- EmployeeSQL/Data_Analysis.sql
+++ EmployeeSQL/Data_Analysis.sql
@@ -16,3 +16,32 @@ ON d.dept_no = m.dept_no
 INNER JOIN employee_info as e
 ON m.emp_no = e.emp_no
 
+--List the department of each employee with the following information: employee number, last name, first name, and department name.
+SELECT e.emp_no, e.dept_no, i.last_name, i.first_name FROM Employee as e
+INNER JOIN Employee_Info as i
+ON e.emp_no = i.emp_no
+INNER JOIN Department as d
+ON e.dept_no = d.dept_no
+
+--List first name, last name, and sex for employees whose first name is "Hercules" and last names begin with "B."
+SELECT first_name, last_name, sex FROM Employee_Info
+WHERE first_name = 'Hercules' AND left(last_name,1) = 'B'
+
+--List all employees in the Sales department, including their employee number, last name, first name, and department name.	 
+SELECT i.emp_no, i.last_name, i.first_name, d.dept_name FROM Employee as e
+INNER JOIN Employee_Info as i
+ON e.emp_no = i.emp_no
+INNER JOIN Department as d
+ON e.dept_no = d.dept_no
+WHERE d.dept_name = 'Sales'		 
+		 
+--List all employees in the Sales and Development departments, including their employee number, last name, first name, and department name.
+SELECT i.emp_no, i.last_name, i.first_name, d.dept_name FROM Employee as e
+INNER JOIN Employee_Info as i
+ON e.emp_no = i.emp_no
+INNER JOIN Department as d
+ON e.dept_no = d.dept_no
+WHERE d.dept_name in ('Sales', 'Development')
+
+
+	
